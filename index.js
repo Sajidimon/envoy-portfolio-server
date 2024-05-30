6fe90850b76cf0551877bbee81453d048d2cc061
sajidimon <imonchowdhury6@gmail.com>
2024-05-30 11:53:51 +0600

    url changed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 //middleware;
 
 app.use(cors({
-    origin: 'http://localhost:5173'
+    origin: 'https://sajidimon-portfolio.web.app'
 }))
 
 app.use(express.json())
